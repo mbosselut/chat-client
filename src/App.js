c4fbd90e5a8b5d240baecfd580008f9a14e65a2f
mbosselut <duchmol7@hotmail.com>
2019-11-15 14:28:15 +0100

    Add and display rooms

--- src/App.js
+++ src/App.js
@@ -1,69 +1,13 @@
-import React, { Component } from 'react';
-import superagent from 'superagent';
+import React, { Fragment, Component } from 'react';
+import { Route } from 'react-router-dom';
+import Rooms from './Rooms';
 
 export default class App extends Component {
-    state = {
-        messages: [],
-        value: ''
-    };
-
-    stream = new EventSource('http://localhost:4000/stream');
-
-    componentDidMount() {
-        this.stream.onmessage = event => {
-            const { data } = event;
-            // Parsing data, as it is received as a string
-            const parsed = JSON.parse(data);
-
-            if (Array.isArray(parsed)) {
-                this.setState({ messages: parsed });
-            } else {
-                const messages = [...this.state.messages, parsed];
-                this.setState({ messages });
-            }
-        };
-    }
-
-    onChange = event => {
-        const { value } = event.target;
-        this.setState({ value });
-    };
-
-    onSubmit = event => {
-        //stops the form from reloading the page
-        event.preventDefault();
-        const { value } = this.state;
-        const url = 'http://localhost:4000/message';
-        superagent
-            .post(url)
-            .send({ message: value })
-            .then(response => console.log(response));
-        this.setState({ value: '' });
-    };
-
-    reset = () => {
-        this.setState({ value: '' });
-    };
-
     render() {
-        const list = this.state.messages.map((message, index) => (
-            <p key={index}>{message}</p>
-        ));
         return (
-            <div>
-                <form onSubmit={this.onSubmit}>
-                    <input
-                        type='text'
-                        onChange={this.onChange}
-                        value={this.state.value}
-                    ></input>
-                    <button type='button' onClick={this.reset}>
-                        Reset
-                    </button>
-                    <button>Submit</button>
-                </form>
-                {list}
-            </div>
+            <Fragment>
+                <Route path='/' component={Rooms} exact></Route>
+            </Fragment>
         );
     }
 }
