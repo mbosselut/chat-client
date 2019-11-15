eb90ef1314c22ebb0b6ac17cffdb86a3d29faa5b
mbosselut <duchmol7@hotmail.com>
2019-11-15 16:25:41 +0100

    Implemented rooms functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Fragment, Component } from 'react';
 import { Route } from 'react-router-dom';
 import Rooms from './Rooms';
+import Room from './Room';
 
 export default class App extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Route path='/' component={Rooms} exact></Route>
-            </Fragment>
-        );
-    }
+	render() {
+		return (
+			<Fragment>
+				<Route path='/' component={Rooms} exact></Route>
+				<Route path='/room/:name' component={Room}></Route>
+			</Fragment>
+		);
+	}
 }
---
+++ src/Room.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import superagent from 'superagent';
+// import { Link } from 'react-router-dom';
+
+export default class Room extends Component {
+	state = {
+		messages: [],
+		value: ''
+	};
+
+	stream = {};
+	// new EventSource('http://localhost:4000/stream');
+
+	componentDidMount() {
+		const { name } = this.props.match.params;
+		const url = `http://localhost:4000/streams/${name}`;
+
+		this.stream = new EventSource(url);
+
+		this.stream.onmessage = event => {
+			// Destructure the data passed to stream.send
+			const { data } = event;
+			console.log(data);
+			// Parse data, as it is received as a string
+			const parsed = JSON.parse(data);
+
+			// Check whether the sent data is an array
+			if (Array.isArray(parsed)) {
+				this.setState({ messages: parsed });
+			} else {
+				const messages = [...this.state.messages, parsed];
+				this.setState({ messages });
+			}
+		};
+	}
+
+	onChange = event => {
+		const { value } = event.target;
+		this.setState({ value });
+	};
+
+	onSubmit = event => {
+		//stops the form from reloading the page
+		event.preventDefault();
+		const { value } = this.state;
+		const { name } = this.props.match.params;
+		const url = `http://localhost:4000/message/${name}`;
+		superagent
+			.post(url)
+			.send({ message: value })
+			.then(response => console.log(response));
+		this.setState({ value: '' });
+	};
+
+	reset = () => {
+		this.setState({ value: '' });
+	};
+
+	render() {
+		const list = this.state.messages.map((message, index) => (
+			<p key={index}>{message}</p>
+		));
+		return (
+			<div>
+				<form onSubmit={this.onSubmit}>
+					<input
+						type='text'
+						onChange={this.onChange}
+						value={this.state.value}
+					></input>
+					<button type='button' onClick={this.reset}>
+						Reset
+					</button>
+					<button>Submit</button>
+				</form>
+				{list}
+			</div>
+		);
+	}
+}
